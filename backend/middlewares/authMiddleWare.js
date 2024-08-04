eefcb872dad1e9635efb50af734ffcac0341d96b
PramodToleti <pramodraina047@gmail.com>
2024-08-05 01:09:40 +0530

    feat: Add authMiddleWare and checkAdmin middlewares for authentication

---
+++ backend/middlewares/authMiddleWare.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const authMiddleWare = (req, res, next) => {
+  const token = req.header("Authorization")?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (e) {
+    res.status(401).json({ error: "Unauthorized" });
+  }
+};
